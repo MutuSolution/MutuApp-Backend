6923d090b9e075e78804ed380cf6cf6f2f73c882
Yunus Gündüz <yns3gndz@hotmail.com.tr>
2024-12-16 00:23:28 +0300

    feat: added token query and controller

---
+++ src/Application/Features/Identity/Queries/GetTokenQuery.cs
@@ -0,0 +1,31 @@
+﻿using Application.Services.Identity;
+using Common.Requests;
+using Common.Responses.Wrappers;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Identity.Queries;
+
+public class GetTokenQuery : IRequest<IResponseWrapper>
+{
+    public TokenRequest TokenRequest { get; set; }
+}
+
+public class GetTokenQueryHandler : IRequestHandler<GetTokenQuery, IResponseWrapper>
+{
+    private readonly ITokenService _tokenService;
+
+    public GetTokenQueryHandler(ITokenService tokenService)
+    {
+        _tokenService = tokenService;
+    }
+
+    public async Task<IResponseWrapper> Handle(GetTokenQuery request, CancellationToken cancellationToken)
+    {
+        return await _tokenService.GetTokenAsync(request.TokenRequest);
+    }
+}
---
+++ src/WebApi/Controllers/Identity/TokenController.cs
@@ -0,0 +1,31 @@
+﻿using Application.Features.Identity.Queries;
+using Common.Requests;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers.Identity
+{
+    [Route("api/[controller]")]
+    public class TokenController : MyBaseController<TokenController>
+    {
+        [HttpPost("get-token")]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetTokenAsync([FromBody] TokenRequest tokenRequest)
+        {
+            var response = await MediatorSender.Send(new GetTokenQuery { TokenRequest = tokenRequest });
+            if (!response.IsSuccessful) return BadRequest(response);
+            return Ok(response);
+        }
+
+        [HttpGet("refresh-token")]
+        public async Task<IActionResult> GetRefreshTokenAsync(
+            [FromBody] RefreshTokenRequest refreshTokenRequest)
+        {
+            var response = await MediatorSender
+                .Send(new GetRefreshTokenQuery { RefreshTokenRequest = refreshTokenRequest});
+            if (!response.IsSuccessful) return BadRequest(response);
+            return Ok(response);
+        }
+    }
+}
