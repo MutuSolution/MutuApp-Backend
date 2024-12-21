02163faf440aea3463d50346ec1f117eb77dd756
Yunus Gündüz <yns3gndz@hotmail.com.tr>
2024-12-21 21:58:51 +0300

    [Feature]: Sorting has happened generic.

--- src/Application/Extensions/LinkExtensions.cs
+++ src/Application/Extensions/LinkExtensions.cs
@@ -10,26 +10,15 @@ namespace Application.Extensions;
 
 public static class LinkExtensions
 {
-    public static IQueryable<Link> Sort(this IQueryable<Link> links, string orderByQeryString)
+    public static IQueryable<Link> Sort(this IQueryable<Link> links, string orderByQueryString)
     {
-        if(string.IsNullOrWhiteSpace(orderByQeryString))
+        if(string.IsNullOrWhiteSpace(orderByQueryString))
             return links.OrderBy(x=> x.Id);
-        var orderParams = orderByQeryString.Trim().Split(',');
-        var propertyInfos = typeof(Link)
-            .GetProperties(BindingFlags.Public | BindingFlags.Instance);
-        var orderQueryBuilder = new StringBuilder();
-        foreach (var param in orderParams)
-        {
-            if (string.IsNullOrWhiteSpace(param)) continue;
-            var propertyFromQueryName = param.Split(' ')[0];
-            var objectProperty = propertyInfos
-                .FirstOrDefault(x => x.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
-            if (objectProperty == null) continue;
-            var direction = param.EndsWith(" desc") ? "descending" : "ascending";
-            orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction}");
-        }
-        var orderQuery = orderQueryBuilder.ToString().TrimEnd(',',' ');
+
+        var orderQuery = OrderQueryBuilder.CreateOrderQuery<Link>(orderByQueryString);
+
         if (orderQuery is null) return links.OrderBy(x => x.Id);
+
         return links.OrderBy(orderQuery);
     }
 }
---
+++ src/Application/Extensions/OrderQueryBuilder.cs
@@ -0,0 +1,36 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Extensions;
+
+public static class OrderQueryBuilder
+{
+    public static string CreateOrderQuery<T>(string orderByQueryString)
+    {
+
+        var orderQueryBuilder = new StringBuilder();
+        var propertyInfos = typeof(T)
+           .GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        var orderParams = orderByQueryString.Trim().Split(',');
+
+        foreach (var param in orderParams)
+        {
+            if (string.IsNullOrWhiteSpace(param)) continue;
+            var propertyFromQueryName = param.Split(' ')[0];
+            var objectProperty = propertyInfos
+                .FirstOrDefault(x => x.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
+            if (objectProperty == null) continue;
+            var direction = param.EndsWith(" desc") ? "descending" : "ascending";
+            orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction}");
+        }
+
+        var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+        return orderQuery;
+    }
+}
